3af3ac4f8a3b3bd33a575b61f724802994ab7788
Talha Turkyolu <turkyolutalha@gmail.com>
2021-12-12 10:47:21 -0600

    added main class

---
+++ src/oop/Main.java
@@ -0,0 +1,26 @@
+package oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //*******OOP-Encapsulation*******//
+
+
+
+
+        //*******OOP-Inheritance*******//
+
+
+
+
+        //*******OOP-Interface-Abstraction*******//
+
+
+
+
+        //*******OOP-Polymorphism*******//
+
+    }
+
+}
